2620b09780933cb05ac7fa7409b370b5e490cf62
dpolozov <dpolozov@gmail.com>
2020-07-13 16:16:36 -0700

    react native dotenv for api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import {YELP_API_KEY} from "@env";
+
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer fuySpBJHr4YQQ8gGBEuyALFSoRcJf5uWjZwlgb8rFpZgEnHJvUtiBPzJU1Kp8tYat7zW7RRZI_6Bf4rvA3SFWrHBywG8KKFGeKah0oqsqUEhHeNgXXJ98CALL38HX3Yx'
+        Authorization: `Bearer ${YELP_API_KEY}`
     }
 });
 
